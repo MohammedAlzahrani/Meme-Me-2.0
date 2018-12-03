3ca80eb957177467237c3158feee480fbb9da121
Mohammed ALZAHRANI <mody@mysystem.local>
2018-12-03 22:19:10 +0300

    add the deletion feature for the table view

--- Meme Me 2.0/AppDelegate.swift
+++ Meme Me 2.0/AppDelegate.swift
@@ -18,6 +18,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         return true
     }
+    // Delete a meme
+    func deleteMeme(index: Int){
+        memes.remove(at: index)
+    }
 
 }
 
--- Meme Me 2.0/SentMemesTableViewController.swift
+++ Meme Me 2.0/SentMemesTableViewController.swift
@@ -10,19 +10,13 @@ import UIKit
 
 class SentMemesTableViewController: UITableViewController {
     @IBOutlet var memesTableView: UITableView!
-    var memes: [Meme]! {
+    var memes: [Meme]!{
         let object = UIApplication.shared.delegate
         let appDelegate = object as! AppDelegate
         return appDelegate.memes
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        //navigationItem.rightBarButtonItem = UIBarButtonItem(title: <#T##String?#>, style: <#T##UIBarButtonItemStyle#>, target: <#T##Any?#>, action: <#T##Selector?#>)
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
         self.navigationItem.title = "Sent Mems"
     }
     @IBAction func newMeme(_ sender: Any) {
@@ -38,12 +32,10 @@ class SentMemesTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return memes.count
     }
 
@@ -64,49 +56,12 @@ class SentMemesTableViewController: UITableViewController {
         detailVC.meme = meme
         navigationController?.pushViewController(detailVC, animated: true)
     }
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        if editingStyle == .delete{
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            appDelegate.deleteMeme(index: indexPath.row)
+            self.memesTableView.reloadData()
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
